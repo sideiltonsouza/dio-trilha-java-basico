a4da0e1732124d6f682a9ec9fdbd6cf40a65cc84
Sideilton Souza <sideiltonsouza@outlook.com>
2025-02-24 16:52:38 -0300

    Finalizado projeto canta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite seu nome: ");
+        String nome =  scanner.nextLine();
+
+        System.out.print("Digite agora o número da sua agencia: ");
+        String agencia =  scanner.nextLine();
+
+        System.out.print("Digite agora o número da sua conta: ");
+        int numero =  scanner.nextInt();
+        
+        System.out.print("Por último o saldo: ");
+        double saldo =  scanner.nextDouble();
+
+
+        System.out.println("---------------------------------");
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
